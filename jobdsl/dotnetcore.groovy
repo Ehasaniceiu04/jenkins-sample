c4f273af5befbf572b7f3e2e3736f819293c3f7f
Ehasanul Hoque <Ehasaniceiu04@users.noreply.github.com>
2019-10-17 16:43:03 +0600

    Update dotnetcore.groovy

--- jobdsl/dotnetcore.groovy
+++ jobdsl/dotnetcore.groovy
@@ -1,14 +1,12 @@
 job('dotnet core job dsl') {
     scm {
-        git('https://github.com/Ehasaniceiu04/shorturl.git') { 
+        git('https://github.com/Ehasaniceiu04/shorturl.git','master') { 
         }
     }
     triggers {
         scm('* * * * *')
     }
     steps {
-        batchFile('dotnet restore')
-        batchFile('dotnet build')
-        batchFile('dotnet test')
+        batchFile('dotnet restore dotnet build dotnet test')
     }
 }
